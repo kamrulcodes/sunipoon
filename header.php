3bccbfaab7171e100ae76bc1264ca2bd0ae165e6
Kamrul Hasan <devKamrul@gmail.com>
2020-03-14 19:35:57 +0600

    Editing header.php for Topbar.

--- header.php
+++ header.php
@@ -25,9 +25,35 @@
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'sunipoon' ); ?></a>
 
 	<header id="masthead" class="site-header">
+    
+        <div id="topbar" class="topbar">
+            <div class="container">
+
+                <?php the_custom_logo(); ?>
+
+                <nav id="site-navigation" class="main-navigation">			
+			        <?php
+			        wp_nav_menu( array(
+				        'theme_location' => 'menu-1',
+				        'menu_id'        => 'primary-menu',
+			        ) );
+			        ?>
+
+                    <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'sunipoon' ); ?></button>
+
+		        </nav><!-- #site-navigation -->
+
+            </div><!-- /.container -->           
+        </div><!-- /.topbar -->
+
+
+
+
+
+
+
 		<div class="site-branding">
 			<?php
-			the_custom_logo();
 			if ( is_front_page() && is_home() ) :
 				?>
 				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
@@ -44,15 +70,6 @@
 			<?php endif; ?>
 		</div><!-- .site-branding -->
 
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'sunipoon' ); ?></button>
-			<?php
-			wp_nav_menu( array(
-				'theme_location' => 'menu-1',
-				'menu_id'        => 'primary-menu',
-			) );
-			?>
-		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
 
 	<div id="content" class="site-content">
