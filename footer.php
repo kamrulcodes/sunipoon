5ff628ef3ae7463ba48b8a9da7eb6ee75f00bc73
Kamrul Hasan <devKamrul@gmail.com>
2020-04-04 23:23:57 +0600

    Footer Styling.

--- footer.php
+++ footer.php
@@ -24,19 +24,21 @@
         </section>
     <?php } ?>
 
-		<section class="site-info container">
-            <span class="copyright">&copy; Copyright <?php echo date("Y"); ?> - <?php bloginfo( 'name' ); ?>.</span>
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'sunipoon' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Powered by %s', 'sunipoon' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'sunipoon' ), 'Sunipoon', '<a href="http://devkamrul.com/">devKamrul.com</a>' );
-				?>
+		<section class="site-info">
+            <div class="container">
+                <span class="copyright">&copy; Copyright <?php echo date("Y"); ?> - <?php bloginfo( 'name' ); ?>. </span>
+    			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'sunipoon' ) ); ?>">
+    				<?php
+    				/* translators: %s: CMS name, i.e. WordPress. */
+    				printf( esc_html__( 'Powered by %s', 'sunipoon' ), 'WordPress' );
+    				?>
+    			</a>
+    			<span class="sep"> | </span>
+    				<?php
+    				/* translators: 1: Theme name, 2: Theme author. */
+    				printf( esc_html__( 'Theme: %1$s by %2$s.', 'sunipoon' ), 'Sunipoon', '<a href="http://devkamrul.com/">devKamrul.com</a>' );
+                    ?>
+            </div>
 		</section><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
