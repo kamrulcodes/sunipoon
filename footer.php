8efcfc0781bb66571e5d4dc7acb4f39180ac2638
Kamrul Hasan <devKamrul@gmail.com>
2020-04-04 16:32:46 +0600

    Widget areas for footer.

--- footer.php
+++ footer.php
@@ -14,24 +14,30 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer">
-    <?php if(!empty (get_theme_mod('custom_footer_logo'))) : ?>    
-        <img src="<?php echo get_theme_mod( 'custom_footer_logo' ); ?>" alt="logo" />
-    <?php endif; ?>
     
-    
-		<div class="site-info">
+    <?php if ( is_active_sidebar( 'footer-sidebar-1' ) || !empty (get_theme_mod('custom_footer_logo')) ) { ?>
+        <section id="footer-widgets" class="footer-widgets container">
+            <div id="footer-logo" class="widget footer-logo">
+                <img src="<?php echo get_theme_mod( 'custom_footer_logo' ); ?>" alt="logo">
+            </div>
+            <?php dynamic_sidebar( 'footer-sidebar-1' ); ?>
+        </section>
+    <?php } ?>
+
+		<section class="site-info container">
+            <span class="copyright">&copy; Copyright <?php echo date("Y"); ?> - <?php bloginfo( 'name' ); ?>.</span>
 			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'sunipoon' ) ); ?>">
 				<?php
 				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'sunipoon' ), 'WordPress' );
+				printf( esc_html__( 'Powered by %s', 'sunipoon' ), 'WordPress' );
 				?>
 			</a>
 			<span class="sep"> | </span>
 				<?php
 				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'sunipoon' ), 'sunipoon', '<a href="http://underscores.me/">Underscores.me</a>' );
+				printf( esc_html__( 'Theme: %1$s by %2$s.', 'sunipoon' ), 'Sunipoon', '<a href="http://devkamrul.com/">devKamrul.com</a>' );
 				?>
-		</div><!-- .site-info -->
+		</section><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
--- functions.php
+++ functions.php
@@ -116,6 +116,16 @@ function sunipoon_widgets_init()
         'before_title' => '<h2 class="widget-title">',
         'after_title' => '</h2>',
     ));
+
+    register_sidebar( array(
+        'name'          => __( 'Footer Widget Area', 'sunipoon' ),
+        'id'            => 'footer-sidebar-1',
+        'description'   => __( 'Widgets in this area will be shown on the footer.', 'sunipoon' ),
+        'before_widget' => '<div id="%1$s" class="widget %2$s">',
+        'after_widget'  => '</div>',
+        'before_title'  => '<h2 class="widget-title">',
+        'after_title'   => '</h2>',
+    ) );
 }
 add_action('widgets_init', 'sunipoon_widgets_init');
 
